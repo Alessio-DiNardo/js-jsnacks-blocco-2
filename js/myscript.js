29a6cdec96918883d8f1a6ef18547c21d9c3a104
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-19 17:22:33 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+//Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+
+
+let array = [];
+let i = 0 ;
+let sum = sum < 50;
+
+while (i < array.length && sum === 50){
+    console.log(array[i]);
+    if (sum < 50){
+    const userNumber = parseInt(prompt("insert a number"));
+    array.push(userNumber);
+}   else if (sum += 50);{
+    i++;
+    }
+}
