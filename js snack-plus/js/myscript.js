b311e70aa8d7ea4cbfee487ced2a1a9a5ee02e25
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-05-23 12:40:58 +0200

    add snack list

---
+++ js snack-plus/js/myscript.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * Generare una lista usando una funzione:
+ * nell'html ci dovrà essere esclusivamente un elemento ul parent,
+ * attraverso una funzione creiamo un singolo elemento html di tipo li
+ * poi ne aggiungiamo una decina all'ul, sempre via js.
+ * 
+ * BONUS:
+ * Se clicchiamo sul li il suo testo verrà sbarrato.
+ */
+
+const ulElement = document.querySelector("ul");
+
+for (let index = 0; index < 10; index++) {
+    ulElement.appendChild(getNewLiElement());
+}
+
+function getNewLiElement(){
+    const newLiElement = document.createElement("li");
+    newLiElement.classList.add("list-item");
+    newLiElement.innetHTML = "Elemento della spesa";
+
+    return newLiElement;
+}
+
+
+
+
+
+
+
+
+
+
+function generateList(li) {
+    document.createElement("li");
+    lists.appendChild(li);
+}
